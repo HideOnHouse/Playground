7e5189e4fb9fa23608aca8c5de989b4507e16fcd
HideOnHouse <fffuro@kookmin.ac.kr>
2021-04-29 15:58:44 +0900

    .

--- switching_fabric.py
+++ switching_fabric.py
@@ -1,12 +1,16 @@
 import random
 
+TABLE = {
+
+}
+
 
 def ip_int2str(ip: int) -> str:
-    pass
+    return ".".join(map(str, [ip >> 24 & 0xFF, ip >> 16 & 0xFF, ip >> 8 & 0xFF, ip & 0xFF]))
 
 
 def main():
-    pass
+    ip = random.randint(0, 2 ** 32)
 
 
 if __name__ == '__main__':
